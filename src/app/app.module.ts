94a5e2473021c3cd85106376a1af3ae301cd108a
luis ramirez <luis.ramirez0001@outlook.com>
2023-07-12 21:45:07 -0500

    [+] se crea directiva ng for para desplegar datos de una lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DespedidaComponent } from './components/despedida/despedida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DespedidaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/despedida/despedida.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-despedida',
+  templateUrl: './despedida.component.html',
+  styleUrls: ['./despedida.component.css']
+})
+export class DespedidaComponent {
+data =[
+  {
+    title: 'Title 1',
+    content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, molestias?',
+    button:'ver mas'
+  },
+  {
+    title: 'Title 1',
+    content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, molestias?',
+    button:'ver mucho  mas'
+  },
+  {
+    title: 'Title 1',
+    content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, molestias?',
+    button:'ver aun mas'
+  }
+];
+}
