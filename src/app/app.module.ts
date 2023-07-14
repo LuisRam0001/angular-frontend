56b5c9a5db1064f998967ac1a23dc31251644564
luis ramirez <luis.ramirez0001@outlook.com>
2023-07-13 19:06:04 -0500

    [+] se crea componente footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent
     
   ],
   imports: [
