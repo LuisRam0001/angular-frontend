a5765ff9e0e79e11ed9e52165f562062d3a9d549
luis ramirez <luis.ramirez0001@outlook.com>
2023-07-17 20:09:22 -0500

    [+]importamos reactivemodule se implementan formularios (validacion)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+loginForm: FormGroup = this.fb.group({
+  email:[
+    '',  // valor predeterminado del campo
+  [
+    Validators.required,
+    Validators.email
+  ]     //validaciones
+  ],
+  password: [
+    '',
+    [
+      Validators.required,
+      Validators.minLength( 8 )
+    ]
+  ]
+});
 
+// inyeccion de dependencias
+
+  constructor(private fb :  FormBuilder) {}
+
+
+  /*  metodos: login */
+  login(){
+    console.group('loginForm');
+    console.info(this.loginForm.value);
+    console.log(this.loginForm.value);
+    console.groupEnd();
+
+  }
 }
